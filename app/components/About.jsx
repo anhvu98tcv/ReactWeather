c0292f9fc115834e0ac0525c9fa4e38fa6322c1c
Anh Vu <anhvu869492@gmail.com>
2017-04-28 10:14:36 +0700

    Add custom css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>Đây là app của VuDz</p>
       <p>Here some of the tool i used</p>
       <ul>
