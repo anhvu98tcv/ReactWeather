5f3c03161e833b9d812f3bf803d1f0f20af22ef3
Anh Vu <anhvu869492@gmail.com>
2017-05-04 10:32:38 +0700

    Completd

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
   getDefaultProps: function() {
@@ -11,20 +13,28 @@ var ErrorModal = React.createClass({
     message: React.PropTypes.string.isrequired
   },
   componentDidMount: function() {
+    var {title, message} = this.props;
+    var modalMarkup = (
+      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+        <h4>{title}</h4>
+        <p>{message}</p>
+        <p>
+          <button className="button hollow" data-close="">
+            Okay
+          </button>
+        </p>
+      </div>
+    )
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
     var modal = new Foundation.Reveal($('#error-modal'));
     modal.open();
   },
   render: function() {
-    var {title, message} = this.props;
+
     return(
-        <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-          <h4>{title}</h4>
-          <p>{message}</p>
-          <p>
-            <button className="button hollow" data-close="">
-              Okay
-            </button>
-          </p>
+        <div>
         </div>
     );
   }
